dd0fd7ef4522b957d7edf8366084fb15b5312cc0
JOE-DP <joe.purvis@hotmail.co.uk>
2022-04-16 13:56:22 +0100

    new level 5 challenge

--- js/level-five.js
+++ js/level-five.js
@@ -1,3 +1,33 @@
+// codewars level 5 challenge - https://www.codewars.com/kata/5581e52ac76ffdea700000c1 - This one is really interesting, looking at cellular automation and undertanding rule 30.
+function rule30(list, n){
+    let rule30Obj = {
+        "000": 0,
+        "001": 1,
+        "010": 1,
+        "011": 1,
+        "100": 1,
+        "101": 0,
+        "110": 0,
+        "111": 0
+    }  
+        // function to run a line of cellular automation
+    function cellLine(list){
+        let resultArr = [];
+        list.push(0, 0);
+        list.unshift(0, 0);
+        for(let i = 1; i < list.length -1; i++){
+        let neighborCells = [list[i-1], list[i], list[i+1]]
+        resultArr.push(rule30Obj[neighborCells.join("")]);
+        }
+  		return resultArr
+    }
+    // function to run the cellLine function n times
+    for(let j = 0; j < n ; j++){
+       list = cellLine(list)
+    }
+    return list
+  }
+
 // codewars level 5 challenge - https://www.codewars.com/kata/525c65e51bf619685c000059/javascript - comparing 2 objects
 
 function cakes(recipe, available) {
@@ -15,4 +45,4 @@ function cakes(recipe, available) {
     
     }
     
-    cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+    // cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
