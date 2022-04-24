f2c8329c61737d03ae5e9dd9e4e77d4631e6112c
JOE-DP <joe.purvis@hotmail.co.uk>
2022-04-24 19:07:47 +0100

    add level 7 solution

--- js/level-six.js
+++ js/level-six.js
@@ -1,5 +1,40 @@
-// codewars level 6 challenge -  https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript sum all numbers in a range, which are multiples of 3 or 5. Additionally negative numbers are 0.
+// codewars level 6 challenge - https://www.codewars.com/kata/57f4ccf0ab9a91c3d5000054/train/javascript - introduction to chmod
 
+function chmodCalculator(perm) {
+    if(!perm.group){
+      perm.group = '---'
+    }
+     if(!perm.user){
+      perm.user = '---'
+    }
+     if(!perm.other){
+      perm.other = '---'
+    }
+    
+    let user = perm.user.split("")
+    let group = perm.group.split("")
+    let other = perm.other.split("")
+    
+    function digit(group){
+      return group.map(item => {
+        if(item == "r"){
+          return 4
+        } else if(item == "w"){
+          return 2
+        } else if(item == "x"){
+          return 1
+        } else {
+          return 0
+        }
+      }).reduce((curr, prev) => curr + prev)
+    }
+    
+    return (`${digit(user)}${digit(group)}${digit(other)}`)
+  }
+
+  
+
+// codewars level 6 challenge -  https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript sum all numbers in a range, which are multiples of 3 or 5. Additionally negative numbers are 0.
 
 function solution(number){
     let arr = []
