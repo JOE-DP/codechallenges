72759d99a55f4f3455c7c5591ef49d78e032d46e
joe purvis <joe.purvis@hotmail.co.uk>
2023-09-25 19:31:31 +0100

    add level 4 JS kata

--- js/level-four.js
+++ js/level-four.js
@@ -3,10 +3,10 @@
 function runoff(voters){
     let votesThreshold = voters.length/2;
     let answer = 0;
-    let newVoters =[...voters] 
+    let newVoters = [...voters] ;
     //function for turning top votes into an object
-    let topObj = function(currentvotes){
-    obj = {}
+    let topVotesObj = function(currentvotes){
+    let obj = {}
     //map votes to obj
     for(let i = 0; i < currentvotes.length; i++){
         if(obj[currentvotes[i][0]]){
@@ -27,19 +27,84 @@ function runoff(voters){
             }
         }
             }
-
-        fiftyCheck(topObj(voters))
-        if(answer == 0){
-            let highestVoters = topObj(newVoters);
-            
+    // todo function which removes all subjects from newVoters which dont appear in anyones top vote
+    function removeNonVotes(){
+        let topVotes = topVotesObj(newVoters), topVotesList = Object.keys(topVotes), subjectList = newVoters[0], unusedSubjects = [];
+        for(let i = 0; i < subjectList.length; i++){
+            if(!topVotesList.includes(subjectList[i])){
+                unusedSubjects.push(subjectList[i]);
+            }
         }
-        console.log(answer)
-    
+        //unused subjects contains an array of 'non voted' subjects whuch need to be removed from the voter arrays (newVoters)
+        for(let i = 0; i < unusedSubjects.length; i++){
+            for(let j = 0; j < newVoters.length; j++){
+                newVoters[j] = newVoters[j].filter((item) => item != unusedSubjects[i])
+            }
+        }
+    } 
+        // function which provides an array of the values to remove from newVoters based on having the fewest votes
+        function findFewestVotes(votesObj){
+            let voteKeys = Object.keys(votesObj)
+            let voteCountsNum = [];
+            for(let i = 0; i < voteKeys.length; i++){
+                voteCountsNum.push(Number(votesObj[voteKeys[i]]));
+            }
+            let leastVoteNumber = Math.min(...voteCountsNum)
+            //any key from votes obj with the leastVoteNumber value need to be returned
+            let valuesToRemove = [];
+            for(let i = 0; i < voteKeys.length; i++){
+                if(votesObj[voteKeys[i]] == leastVoteNumber){
+                    valuesToRemove.push(voteKeys[i]);
+                }
+            }
+            
+            console.log(valuesToRemove.length, voteKeys.length)
+
+            if(valuesToRemove.length == voteKeys.length || valuesToRemove.length == 0){
+                answer = 'undefined';
+            }
+            return valuesToRemove
+        }
+        // function which removes subjects from newVoters - recieved as an array
+        function removeFromVoters(valuesToRemove){
+            for(let i = 0; i < valuesToRemove.length; i++){
+                for(let j = 0; j < newVoters.length; j++){
+                    newVoters[j] = newVoters[j].filter((item) => item != valuesToRemove[i])
+                }
+            }
+        }
+
+        //first remove non top votes from the whole array      
+        removeNonVotes()
+        //then check to see if any of the top votes have above 50% majority using the updated voter arrays and update 'answer' if so
+        //check for 50%
+        fiftyCheck(topVotesObj(newVoters))
+        
+        if(answer == 0){
+            let fewestVotes = findFewestVotes(topVotesObj(newVoters))
+            removeFromVoters(fewestVotes)
+            fiftyCheck(topVotesObj(newVoters))
+        }
+
+        if(answer == 0){
+            let fewestVotes = findFewestVotes(topVotesObj(newVoters))
+            removeFromVoters(fewestVotes)
+            fiftyCheck(topVotesObj(newVoters))
+        }
+
+        if(answer == 0){
+            let fewestVotes = findFewestVotes(topVotesObj(newVoters))
+            removeFromVoters(fewestVotes)
+            fiftyCheck(topVotesObj(newVoters))
+        }
+        
+        
+        if(answer == 'undefined'){
+          return undefined
+        }
+        return answer
       }
 
-runoff([ [ 'd', 'a', 'e', 'b', 'c' ],
-[ 'b', 'e', 'd', 'c', 'a' ],
-[ 'e', 'a', 'c', 'b', 'd' ],
-[ 'e', 'd', 'a', 'b', 'c' ],
-[ 'd', 'b', 'a', 'e', 'c' ] ])
+
+
 
